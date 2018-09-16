79b37cbc1229417deace29183d73aabbab06ea3c
noy leibovitch <noylei31@gmail.com>
2018-09-16 12:53:41 +0300

    code before dbug

--- ships_cleaner.py
+++ ships_cleaner.py
@@ -5,7 +5,7 @@ import plotly.plotly as py
 import datetime
 import dateutil.parser
 import time
-from util_functions
+from util_functions import parse_property_key, turn_property_to_array
 
 #
 # def turn_property_to_array(property_key, mongo_collection):
@@ -15,50 +15,72 @@ from util_functions
 #         new_prorperty_array.append(element[property_key])
 #
 #     return new_prorperty_array
+
+# recieving from user score cutoff
+# in the future we can work either with: a GUI, WEB GUI, questions and pop ups, or with a config file
 try:
     score_cutoff = float(sys.argv[1])
 except IndexError:
     score_cutoff = 0
 
-
-def parse_property_key(property_key):
-    newsted_key_array = property_key.split('.')
-    full_string = []
-    for word in newsted_key_array:
-        full_string.append('["' + word + '"]')
-
-    full_string = ''.join(full_string)
-    return full_string
-
-
-def turn_property_to_array(property_key, mongo_collection):
-    cursor = mongo_collection.find({}, {property_key: 1})
-    new_prorperty_array = []
-    full_string = parse_property_key(property_key)
-    for element in cursor:
-        exec('new_prorperty_array.append(element' + full_string + ')')
-
-    return new_prorperty_array
-
-
+#connecting to client
 client = pymongo.MongoClient('mongodb://localhost:27017/')
+
+#creating the new work db for the session
 name_list = client.database_names()
 if 'working_db' not in name_list:
     client.admin.command('copydb', fromdb='planet', todb='working_db', upsert=True)
 
 db = client.working_db
-print(score_cutoff)
-score_query = {"properties.score": {"$lt": score_cutoff}, "properties.cloud_cover": 0}
+score_query = {"$or": [{"properties.score": {"$lt": score_cutoff}}, {"properties.cloud_cover": 1}]}
 ships_collection = db.ships
 response = ships_collection.delete_many(score_query)
-print(response.deleted_count)
-scores_array = turn_property_to_array("properties.score", ships_collection)
+print(str(response.deleted_count) + " ships were deleted, as they were either covered by clouds or had a low score")
 
+#create new properties for the entries: satellite name and time
+projection = {"geometry": 1, "properties.source_item": 1,
+              "properties.observed": 1, "id": 1}
+all_ships = ships_collection.find({}, projection)
+iteration = 0
+for ship in all_ships:
+    print(ship)
+    # print(iteration)
+    # iteration += 1
+    # current_satellite = ship["properties"]["source_item"].split('_')
+    # currentTimeTuple = dateutil.parser.parse(ship["properties"]["observed"]).timetuple()
+    # ships_collection.update_one({"_id": ship["_id"]}, {"$set": {"time_in_seconds": time.mktime(currentTimeTuple)}})
+    # ships_collection.update_one({"_id": ship["_id"]}, {"$set": {"satellite_name": current_satellite[2]}})
 
-plt.hist(scores_array, bins=20)
-plt.title("score distribution")
-plt.gca().invert_xaxis()
-plt.show()
+# cursor2 = ships_collection.find({"satellite_name": {'$exists': True}}, {"satellite_name": 1,
+#                                                                         "time_in_seconds": 1,
+#                                                                         "geometry": 1})
+# cursor2.sort([("satellite_name", pymongo.ASCENDING), ("time_in_seconds", pymongo.ASCENDING)])
+# total_deleted_ships_number = 0
+# current_deletion_number = 0
+# while True:
+#     for ship in cursor2:
+#         delete_copies_query = {"time_in_seconds": {'$lte': ship['time_in_seconds'] + 3},
+#                                "satellite_name": ship['satellite_name'],
+#                                "geometry": {"$geoIntersects": {"$geometry": ship['geometry']}}}
+#         current_deletion_number = ships_collection.delete_many(delete_copies_query).deleted_count
+#         print(current_deletion_number)
+#         total_deleted_ships_number += current_deletion_number
+#     if current_deletion_number == 0:
+#         break
+#     else:
+#         cursor2 = ships_collection.find({"satellite_name": {'$exists': True}}, {"satellite_name": 1,
+#                                                                                 "time_in_seconds": 1,
+#                                                                                 "geometry": 1})
+#         cursor2.sort([("satellite_name", pymongo.ASCENDING), ("time_in_seconds", pymongo.ASCENDING)])
+#
+#
+# print(total_deleted_ships_number)
+# #plotting histogram of ship scores
+# # scores_array = turn_property_to_array("properties.score", ships_collection)
+# # plt.hist(scores_array, bins=20)
+# # plt.title("score distribution")
+# # plt.gca().invert_xaxis()
+# # plt.show()
 
 client.drop_database('working_db')
 # fig = plt.gcf()
@@ -69,8 +91,7 @@ client.drop_database('working_db')
 
 
 # query = {"properties.cloud_cover":0}
-# projection = {"geometry": 1, "properties.cloud_cover": 1, "properties.source_item": 1,
-#               "properties.observed": 1, "id": 1}
+
 # cursor = ships_collection.find(query, projection)
 # counter = 0
 # datesArray = []
@@ -81,8 +102,7 @@ client.drop_database('working_db')
 #     ships_collection.update_one({"_id": ship["_id"]}, {"$set": {"time_in_seconds": time.mktime(currentTimeTuple)}})
 #     ships_collection.update_one({"_id": ship["_id"]}, {"$set": {"satellite_name": current_satellite[2]}})
 #
-# cursor2 = ships_collection.find({"satellite_name": {'$exists': True}}, {"satellite_name": 1, "time_in_seconds": 1})
-# cursor2.sort([("satellite_name", pymongo.ASCENDING), ("time_in_seconds", pymongo.ASCENDING)])
+
 #
 # for element in cursor2:
 #     print(element)
--- util_functions.py
+++ util_functions.py
@@ -1,23 +1,22 @@
-import pymongo
-
 def parse_property_key(property_key):
-    newsted_key_array = property_key.split()
-    full_string = newsted_key_array[0]
+    newsted_key_array = property_key.split('.')
+    full_string = []
+    for word in newsted_key_array:
+        full_string.append('["' + word + '"]')
 
-    for word in newsted_key_array [1:len(newsted_key_array)]:
-        full_string.append('[' + word + ']')
-
+    full_string = ''.join(full_string)
     return full_string
 
-def turn_property_to_array(property_key, mongo_collection):
-    cursor = mongo_collection.find({}, {property_key: 1})
+
+git status
+cursor = mongo_collection.find({}, {property_key: 1})
     new_prorperty_array = []
+    full_string = parse_property_key(property_key)
     for element in cursor:
-        exec('new_prorperty_array.append(' + parse_property_key(property_key) + ')')
+        exec('new_prorperty_array.append(element' + full_string + ')')
 
     return new_prorperty_array
 
-def haylow():
-    print('great sucess!!')
+
 
 
